7374157638a53c13a68bb2aa78c2e40f0ed8fed7
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-06-20 22:38:48 +0200

    cointarnes

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Container=({children})=>{
+    return (
+        <div className='container'>
+            {children}
+        </div>
+    )
+}
+
+export default Container
